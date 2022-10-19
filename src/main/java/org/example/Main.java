3953e9a0a54a063da5837c1e7718428fa51efff1
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-10-20 00:17:04 +0300

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,42 +17,53 @@ public class Main {
         var usersTeenagers = new ArrayList<>();
         var usersNameFinishO = new ArrayList<>();
         var usersWorkAge = new ArrayList<>();
+        var usersNameWithA = new ArrayList<>();
+        var usersFullAge = new ArrayList<>();
 
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/my_db","root","1234");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/my_db", "root", "1234");
             statement = con.createStatement();
 
-        var resultSetTeenagers = statement.executeQuery(SELECT_ALL_USERS_TEENAGERS);
-            while (resultSetTeenagers.next())
-            {
-                User user = new User();
-                user.setFirstName(resultSetTeenagers.getString("firstName"));
-                user.setLastName(resultSetTeenagers.getString("lastName"));
-                user.setAge(resultSetTeenagers.getInt("age"));
-                user.setLastName(resultSetTeenagers.getString("gender"));
-                usersTeenagers.add(user);
+            var resultSetTeenagers = statement.executeQuery(SELECT_ALL_USERS_TEENAGERS);
+            while (resultSetTeenagers.next()) {
+                usersTeenagers.add(new User(resultSetTeenagers.getString("firstName"), resultSetTeenagers.getString("lastName"),
+                        resultSetTeenagers.getInt("age"), resultSetTeenagers.getString("gender")));
             }
 
-         var resultSetNameFinishO = statement.executeQuery(SELECT_ALL_USERS_NAME_FINISHED_O);
-            while (resultSetNameFinishO.next())
-            {
-                User user = new User();
-                user.setFirstName(resultSetNameFinishO.getString("firstName"));
-                user.setLastName(resultSetNameFinishO.getString("lastName"));
-                user.setAge(resultSetNameFinishO.getInt("age"));
-                user.setLastName(resultSetNameFinishO.getString("gender"));
-                usersNameFinishO.add(user);
+            var resultSetNameFinishO = statement.executeQuery(SELECT_ALL_USERS_NAME_FINISHED_O);
+            while (resultSetNameFinishO.next()) {
+                usersNameFinishO.add(new User(resultSetNameFinishO.getString("firstName"), resultSetNameFinishO.getString("lastName"),
+                        resultSetNameFinishO.getInt("age"), resultSetNameFinishO.getString("gender")));
             }
 
             var resultSetWorkAge = statement.executeQuery(SELECT_ALL_USERS_WORK_AGE);
-            while (resultSetWorkAge.next())
-            {
+            while (resultSetWorkAge.next()) {
                 User user = new User();
                 user.setFirstName(resultSetWorkAge.getString("firstName"));
                 user.setLastName(resultSetWorkAge.getString("lastName"));
                 user.setAge(resultSetWorkAge.getInt("age"));
                 user.setLastName(resultSetWorkAge.getString("gender"));
                 usersWorkAge.add(user);
+            }
+
+            var resultSetNameWithA = statement.executeQuery(SELECT_ALL_USERS_NAME_WITH_A);
+            while (resultSetNameWithA.next()) {
+                User user = new User();
+                user.setFirstName(resultSetNameWithA.getString("firstName"));
+                user.setLastName(resultSetNameWithA.getString("lastName"));
+                user.setAge(resultSetNameWithA.getInt("age"));
+                user.setLastName(resultSetNameWithA.getString("gender"));
+                usersNameWithA.add(user);
+            }
+
+            var resultSetFullAge = statement.executeQuery(SELECT_ALL_USERS_FULL_AGE);
+            while (resultSetFullAge.next()) {
+                User user = new User();
+                user.setFirstName(resultSetFullAge.getString("firstName"));
+                user.setLastName(resultSetFullAge.getString("lastName"));
+                user.setAge(resultSetFullAge.getInt("age"));
+                user.setLastName(resultSetFullAge.getString("gender"));
+                usersNameWithA.add(user);
             }
 
         } catch (SQLException throwable) {
